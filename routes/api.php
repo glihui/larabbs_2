544ab63b1ce7dfe05880185f15da2cec8dc1f937
guolihui <15919476778@163.com>
2018-05-10 14:31:33 +0000

    topic delete api

--- routes/api.php
+++ routes/api.php
@@ -71,6 +71,9 @@ $api->version('v1', [
 
             $api->patch('topics/{topic}', 'TopicsController@update')
                 ->name('api.topics.update');
+
+            $api->delete('topics/{topic}', 'TopicsController@destroy')
+                ->name('api.topics.destroy');
         });
 
     });
