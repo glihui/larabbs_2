92ad5fd27313d5a9d7053bc22006fc20ce81d473
guolihui <15919476778@163.com>
2018-05-11 15:41:25 +0000

    replies store

--- routes/api.php
+++ routes/api.php
@@ -82,6 +82,8 @@ $api->version('v1', [
 
             $api->delete('topics/{topic}', 'TopicsController@destroy')
                 ->name('api.topics.destroy');
+
+            $api->post('topics/{topic}/replies', 'RepliesController@store')->name('api.topics.replies.store');
         });
 
     });
