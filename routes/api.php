4427d278f9c757f852a3ab1e91bfbf2a520310ca
guolihui <15919476778@163.com>
2018-05-13 07:43:06 +0000

    actived users

--- routes/api.php
+++ routes/api.php
@@ -72,6 +72,9 @@ $api->version('v1', [
         $api->get('links', 'LinksController@index')
             ->name('api.links.index');
 
+        $api->get('actived/users', 'UsersController@activedIndex')
+            ->name('api.actived.users.index');
+
         //需要 token 验证的接口
         $api->group(['middleware' => 'api.auth'], function($api) {
             $api->get('user', 'UsersController@me')
