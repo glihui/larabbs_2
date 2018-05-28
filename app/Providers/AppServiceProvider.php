a6a0e28a12afd320919fa6cb726be56ebe84ff9f
guolihui <15919476778@163.com>
2018-05-28 13:50:34 +0000

    add

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\Schema;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,6 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
 	{
+
+        Schema::defaultStringLength(191);
+
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
 		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
