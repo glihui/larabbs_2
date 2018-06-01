2635e0d72ffc4ab5578975551af6adb973bb313d
guolihui <15919476778@163.com>
2018-06-01 14:29:38 +0000

    add

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -10,7 +10,7 @@ class UserObserver
     {
         // 这样写扩展性更高，只有空的时候才指定默认头像
         if (empty($user->avatar)) {
-            $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+            $user->avatar = 'https://hello.guolh.com/uploads/images/avatars/201806/01/7_1527862791_EBum4btIS3.jpeg';
         }
     }
 }
